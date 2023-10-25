a7850851dd099ccd96331546fd5fd987e047fc74
Stilic <lateman-jpeg@outlook.fr>
2023-10-25 22:36:15 +0200

    funny

--- src/Main.hx
+++ src/Main.hx
@@ -5,12 +5,17 @@ class Main
 	static function main()
 	{
 		Rl.initWindow(1280, 720, "KeyNight");
+		Rl.setTargetFPS(60);
+
+		var image = Rl.loadTexture("assets/images/cat.jpg");
 
 		while (!Rl.windowShouldClose())
 		{
 			Rl.beginDrawing();
 			Rl.clearBackground(Rl.Colors.WHITE);
-			Rl.drawText("Congrats! You created your first window!", 190, 200, 20, Rl.Colors.LIGHTGRAY);
+
+			Rl.drawTexture(image, Math.floor(1280/2 - image.width/2), Math.floor(720/2 - image.height/2), Rl.Colors.WHITE);
+
 			Rl.endDrawing();
 		}
 
